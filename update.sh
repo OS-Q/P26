e0cbf84eed3d5b14b4969dd92ff47776ac61b279
qitas <qitas@qitas.cn>
2021-04-15 18:04:12 +0800

    fix

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+# Initialize any submodules.
+git submodule update --init --recursive
+
+cp -rf ../boards .
+cp -rf ../builder .
+cp -rf ../extend .
+cp -f ../link.json .
+cp -f ../link.py .
+
+git add .
+git commit -am "update by github action"
+git push
